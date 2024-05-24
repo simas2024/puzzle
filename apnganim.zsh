92333b8fc160f91d552745cbab70463e1295a21f
simas2024 <info@simas.app>
2024-05-24 14:02:50 +0200

    Update apnganim.zsh
    Fix Darwin OS detection

--- apnganim.zsh
+++ apnganim.zsh
@@ -8,6 +8,7 @@ local tsdir=$opts[-tsdir]
 
 local pngframes=("${(@f)$(printf '%s\n' $tsdir/frame_puzzle_*.png(.oL))}")
 local webpframes=()
+local os_type="$(uname -s)"
 
 if [[ "$os_type" == "Darwin" ]]; then
     for png_frame_file in $pngframes; do
@@ -16,8 +17,6 @@ if [[ "$os_type" == "Darwin" ]]; then
         apngframes+=( "$png_frame_file" 200 )
     done
 fi
-
-local os_type="$(uname -s)"
 
 if [[ "$os_type" == "Linux" ]]; then
     # set delay (see https://manpages.ubuntu.com/manpages/kinetic/man1/apngasm.1.html)
