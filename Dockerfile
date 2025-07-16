afcf10981b5463af01e8f26d86d3f0c5fb446c86
Markus Schmid <170698822+simas2024@users.noreply.github.com>
2025-07-16 19:58:10 +0200

    Fix: use apngasm v3.1.10 from source and remove platform specific logic

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,27 @@ RUN --mount=type=cache,target=/var/cache/apt \
     apt-get update && apt-get install -y \
     zsh \
     imagemagick \
-    apngasm \
     webp \
+    wget \
+    unzip \
+    git \
+    cmake libpng-dev libboost-program-options-dev libboost-regex-dev libboost-system-dev libboost-filesystem-dev build-essential \
     fonts-dejavu-core
 
-RUN mkdir /app
+# Build apngasm library and CLI manually
+RUN rm -rf /tmp/apngasm/ \
+ && git clone https://github.com/apngasm/apngasm.git --branch 3.1.10 --single-branch /tmp/apngasm \
+ && mkdir -p /tmp/apngasm/build \
+ && cd /tmp/apngasm/build \
+ && cmake ../ \
+ && make -j$(nproc) \
+ && cp lib/libapngasm.so /usr/local/lib/ \
+ && echo "/usr/local/lib" > /etc/ld.so.conf.d/apngasm.conf \
+ && ldconfig \
+ && cp cli/apngasm /usr/local/bin/ \
+ && cd / && rm -rf /tmp/apngasm
 
+RUN mkdir /app
 WORKDIR /app
 
 COPY . /app
--- apnganim.zsh
+++ apnganim.zsh
@@ -7,21 +7,11 @@ zparseopts -D -E -A opts tsdir:=opts
 local tsdir=$opts[-tsdir]
 
 local pngframes=("${(@f)$(printf '%s\n' $tsdir/frame_puzzle_*.png(.oL))}")
-local webpframes=()
-local os_type="$(uname -s)"
 
-if [[ "$os_type" == "Darwin" ]]; then
-    for png_frame_file in $pngframes; do
-        # set delay
-        # 200ms delay (macOS)
-        apngframes+=( "$png_frame_file" 200 )
-    done
-fi
+for png_frame_file in $pngframes; do
+    # set delay
+    # 200ms delay (macOS)
+    apngframes+=( "$png_frame_file" 200 )
+done
 
-if [[ "$os_type" == "Linux" ]]; then
-    # set delay (see https://manpages.ubuntu.com/manpages/kinetic/man1/apngasm.1.html)
-    # 1s / 5 = 200ms delay (Linux/Ubuntu)
-    apngasm "$tsdir/done_puzzle.apng.png" $tsdir/frame_puzzle_0000.png 1 5
-else
-    apngasm -o "$tsdir/done_puzzle.apng.png" "${apngframes[@]}"
-fi
+apngasm -o "$tsdir/done_puzzle.apng.png" "${apngframes[@]}"
